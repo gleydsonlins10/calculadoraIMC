5fcccca2a1dc7c0fb36617471f69cff760d72b1c
gleydson <gleydsonlins10@gmail.com>
2020-04-23 09:30:26 -0300

    Ajuste nos formulários

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,11 @@ class _HomeState extends State<Home> {
   String _infoText = "Informe seus Dados";
 
   void _resetFields() {
-    weightController.text = "";
-    heightController.text = "";
-    _infoText = "Informe seus Dados";
+    setState(() {
+      weightController.text = "";
+      heightController.text = "";
+      _infoText = "Informe seus Dados";
+    });
   }
 
   void calculate(){
@@ -32,6 +34,16 @@ class _HomeState extends State<Home> {
       double imc = wight / (height * height);
       if(imc < 18){
         _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      }else if(imc >= 18.6 && imc < 24.9){
+        _infoText = "Peso ideal (${imc.toStringAsPrecision(3)})";
+      }else if(imc >= 24.9 && imc < 29.9){
+        _infoText = "Levemente acima do Peso (${imc.toStringAsPrecision(3)})";
+      }else if(imc >= 29.9 && imc < 34.9){
+        _infoText = "Obesidade Grau 1(${imc.toStringAsPrecision(3)})";
+      }else if(imc >= 34.9 && imc < 39.9){
+        _infoText = "Obesidade Grau 2 (${imc.toStringAsPrecision(3)})";
+      }else if(imc >= 39.9){
+        _infoText = "Obesidade Grau 3 (${imc.toStringAsPrecision(3)})";
       }
 
       print(imc);
